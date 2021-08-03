e48635636dc6eb42b33c4dd3a9c504526b12090d
Mark Anthony Calvario <mac2492@fb.com>
2021-08-03 15:50:51 -0700

    [UniFlip][Feature 87] User can now login with their email address they signed up with

--- UniFlip/View Contollers/LoginViewController.m
+++ UniFlip/View Contollers/LoginViewController.m
@@ -37,15 +37,30 @@
 - (IBAction)didTapLogin:(id)sender {
     NSString *username = self.usernameOrEmailLabel.text;
     NSString *password = self.passwordLabel.text;
-    [PFUser logInWithUsernameInBackground:username password:password
-      block:^(PFUser *user, NSError *error) {
-        if (user) {
-            [self performSegueWithIdentifier:@"LoginToHomeSegue" sender:nil];
-            
-        } else {
-            [self displayLoginError];
-        }
+    
+    PFQuery *query = [PFUser query];
+        [query whereKey:@"email" equalTo:username];
+        [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error){
+            if (objects.count > 0) {
+                PFObject *object = [objects objectAtIndex:0];
+                NSString *username = [object objectForKey:@"username"];
+                [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser* user, NSError* error){
+                    user ? [self performSegueWithIdentifier:@"LoginToHomeSegue" sender:nil] : [self displayLoginError];
+                }];
+            }else{
+                [PFUser logInWithUsernameInBackground: username password:password block:^(PFUser* user, NSError* error){
+                    user ? [self performSegueWithIdentifier:@"LoginToHomeSegue" sender:nil] : [self displayLoginError];
+                }];
+            }
     }];
+}
+- (BOOL)validateEmailWithString:(NSString*)checkString{
+    BOOL stricterFilter = NO;
+    NSString *stricterFilterString = @"[A-Z0-9a-z\\._%+-]+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2,4}";
+    NSString *laxString = @".+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2}[A-Za-z]*";
+    NSString *emailRegex = stricterFilter ? stricterFilterString : laxString;
+    NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
+    return [emailTest evaluateWithObject:checkString];
 }
 -(void) displayLoginError{
     UIAlertController *loginErrorAlert = [UIAlertController alertControllerWithTitle:@"Login Unsuccessful"
